028dba4ecc181b9cba9d4a7e01996049ef3e5ca4
Nikola Cirkovic <cirkovic.developer@gmail.com>
2024-01-31 22:57:30 +0100

    Support route for lead details

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LeadsIndex from "./routes/leads/LeadsIndex";
+import LeadDetails from "./routes/leads/LeadDetails";
 const router = createBrowserRouter([
   {
     path: "/leads",
     element: <LeadsIndex />,
+  },
+  {
+    path: "/leads/:id/details",
+    element: <LeadDetails />,
   },
 ]);
 ReactDOM.createRoot(document.getElementById("root")!).render(
